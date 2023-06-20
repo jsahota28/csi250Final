6034db67656695220a873fdd3fa7ab5c969ab9f9
jsahota28 <121974422+jsahota28@users.noreply.github.com>
2023-06-20 11:50:31 -0700

    Add files via upload

---
+++ ComputerVM.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ComputerStoreFinalProject
+{
+    public class ComputerVM
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+        [Required]
+        [Display(Name = "Computer Model")]
+        public string ComputerModel { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(7,2)")]
+        public decimal Price { get; set; }
+
+        //Navigation properties
+        //Each product has a collection of ComputerOrders associated with it.
+
+        //Property to store the filename of a product image
+        //This is the string that shows
+        //Set this file to take in only Images
+        [FileExtensions(Extensions = ".jpg,.png,.jpeg", ErrorMessage = "Images only")]
+        public string ProductImageString { get; set; }
+
+        
+        //This is the actual picture of the computer
+        public IFormFile ProductImageFile { get; set; }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,69 @@
+using ComputerStoreFinalProject.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputerStoreFinalProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            //Adding our DbContext to Services
+            services.AddDbContext<ComputerStoreDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ComputerStoreDbConnection")));
+            //This is where we register Identity with the services collection
+            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ComputerStoreDbContext>();//(options => options.SignIn.RequireConfirmedAccount = true);
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication(); //This makes sure you are you //This has to be above UseAuthorization.
+            app.UseAuthorization(); //This makes sure if you are allowed to do what you are about to do (e.g. delete Computers/Students).
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            });
+        }
+    }
+}
